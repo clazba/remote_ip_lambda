76a7cbaab788df96d7e821a4bc0b640a16872cc5
clazba <31473290+clazba@users.noreply.github.com>
2018-04-30 17:07:53 +0100

    back to one file

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # app.py
 import os
 from flask import Flask, Response, json, request
-from rds_db import build_db build_response
+import logging
+import pymysql.cursors
+from dotenv import load_dotenv, find_dotenv
 
 app = Flask(__name__)
 
@@ -75,7 +77,56 @@ def user():
 @app.route('/build', methods=["GET"])
 def build():
     return build_db()
+# first, load your env file, replacing the path here with your own if it differs
+# when using the local database make sure you change your path  to .dev.env, it should work smoothly.
+load_dotenv()
 
+# we need to instantiate the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+#dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+# update environment just in case
+#os.environ.update(dotenv)
+# set globals
+RDS_HOST = os.getenv("DB_HOST")
+RDS_PORT = int(os.getenv("DB_PORT", 3306))
+NAME = os.getenv("DB_USERNAME")
+PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+
+def connect():
+    try:
+        cursor = pymysql.cursors.DictCursor
+        conn = pymysql.connect(RDS_HOST, user=NAME, passwd=PASSWORD, db=DB_NAME, port=RDS_PORT, cursorclass=cursor, connect_timeout=5)
+        logger.info("SUCCESS: connection to RDS successful")
+        return(conn)
+    except Exception as e:
+        logger.exception("Database Connection Error")
+
+# Function to build the user table
+def build_db():
+    conn = connect()
+    query = "create table User (ID varchar(255) NOT NULL, firstName varchar(255) NOT NULL, lastName varchar(255) NOT NULL, email varchar(255) NOT NULL, PRIMARY KEY (ID))"
+    try:
+        with conn.cursor() as cur:
+            # just in case it doesn't work the first time let's drop the table if it exists
+            cur.execute("drop table if exists User")
+            cur.execute(query)
+            conn.commit()
+    except Exception as e:
+        logger.exception(e)
+        response = Respone(json.dumps({"status": "error", "message": "could not build table"}), 500)
+    finally:
+        cur.close()
+        conn.close()
+    response = Response(json.dumps({"status": "success"}), 200)
+    return response
+
+# Posting to the database with form data. First, let's break out our response building into a function:
+def build_response(resp_dict, status_code):
+    response = Response(json.dumps(resp_dict), status_code)
+    return response
 # include this for local dev
 if __name__ == '__main__':
     app.run()
