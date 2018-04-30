0a456811337c9268364596c62245f183d3a0b1fc
clazba <camerinorama@gmail.com>
2018-04-30 10:58:33 +0100

    Create project

---
+++ app.py
@@ -0,0 +1,56 @@
+# app.py
+import os
+import logging
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, Response, json, request
+
+app = Flask(__name__)
+
+# here is how we are handling routing with flask:
+@app.route('/')
+def index():
+    connect()
+    return "Hello World!", 200
+
+# Adding a post/get route is pretty straightforward with flask, let's add one for getting a fake user
+@app.route('/user', methods=["GET", "POST"])
+def user():
+    resp_dict = {}
+    if request.method == "GET":
+        resp_dict = {"first_name": "John", "last_name": "doe"}
+    if request.method == "POST":
+        data = request.form
+        first_name = data.get("first_name", "")
+        last_name = data.get("last_name", "")
+        email = data.get("email", "")
+        resp_dict = {"first_name": first_name, "last_name": last_name, "email": email}
+    response = Response(json.dumps(resp_dict), 200)
+    return response
+
+# first, load your env file, replacing the path here with your own if it differs
+# when using the local database make sure you change your path  to .dev.env, it should work smoothly.
+dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+# update environment just in case
+os.environ.update(dotenv)
+# set globals
+RDS_HOST = os.environ.get("DB_HOST")
+RDS_PORT = int(os.environ.get("DB_PORT", 3306))
+NAME = os.environ.get("DB_USERNAME")
+PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+
+# we need to instantiate the logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+def connect():
+    try:
+        cursor = pymysql.cursors.DictCursor
+        conn = pymysql.connect(RDS_HOST, user=NAME, passwd=PASSWORD, db=DB_NAME, port=RDS_PORT, cursorclass=cursor, connect_timeout=5)
+        logger.info("SUCCESS: connection to RDS successful")
+        return(conn)
+    except Exception as e:
+        logger.exception("Database Connection Error")
+
+# include this for local dev
+if __name__ == '__main__':
+    app.run()
